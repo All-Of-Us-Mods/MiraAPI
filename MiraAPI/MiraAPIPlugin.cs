0271430647637e3dcb83c192c7c1c70433e5f25a
ang-xd <angxdxdd@gmail.com>
2024-05-13 17:08:27 -0400

    Initial commit

---
+++ MiraAPI/MiraAPIPlugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Reactor;
+using Reactor.Networking;
+using Reactor.Networking.Attributes;
+
+namespace MiraAPI;
+
+[BepInAutoPlugin("dev.xtracube.launchpad", "MiraAPI")]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+[ReactorModFlags(ModFlags.RequireOnAllClients)]
+public partial class MiraAPIPlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+
+    public override void Load()
+    {
+        Harmony.PatchAll();
+    }
+}
