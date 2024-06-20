2affea8c968edb39d88a7fae90e988f2c7a91ea0
ang-xd <angxdxdd@gmail.com>
2024-06-20 17:47:47 -0400

    Redesign

--- MiraAPI/Patches/Options/GameSettingMenuPatches.cs
+++ MiraAPI/Patches/Options/GameSettingMenuPatches.cs
@@ -17,55 +17,48 @@ public static class GameSettingMenuPatches
     [HarmonyPatch(nameof(GameSettingMenu.Start))]
     public static void StartPrefix(GameSettingMenu __instance)
     {
+        __instance.transform.FindChild("GameSettingsLabel").gameObject.SetActive(false);
+
         Transform helpThing = __instance.transform.FindChild("What Is This?");
         GameObject tmpText = GameObject.Instantiate(helpThing.transform.FindChild("InfoText"), helpThing.parent).gameObject;
 
         tmpText.GetComponent<TextTranslatorTMP>().Destroy();
         tmpText.name = "SelectedMod";
-        tmpText.transform.localPosition = new Vector3(-3.3382f, -0.4019f, -2f);
+        tmpText.transform.localPosition = new Vector3(-3.3382f, 1.5399f, -2);
 
         TextMeshPro tmp = tmpText.GetComponent<TextMeshPro>();
-        tmp.text = "Mira API";
-        tmp.fontSizeMax = 3;
-        tmp.fontSize = 3;
+        tmp.text = "<b>Default</b>";
+        tmp.fontSizeMax = 3.2f;
 
         tmp.alignment = TextAlignmentOptions.Center;
 
         GameObject nextButton = GameObject.Instantiate(__instance.BackButton, __instance.BackButton.transform.parent).gameObject;
-        nextButton.transform.localPosition = new Vector3(-2.2663f, -0.4037f, -25f);
-        nextButton.name = "NextButton";
+        nextButton.transform.localPosition = new Vector3(-2.2663f, 1.5272f, -25f);
+        nextButton.name = "RightArrowButton";
         nextButton.transform.FindChild("Inactive").gameObject.GetComponent<SpriteRenderer>().sprite = MiraAssets.NextButton.LoadAsset();
         nextButton.transform.FindChild("Active").gameObject.GetComponent<SpriteRenderer>().sprite = MiraAssets.NextButtonActive.LoadAsset();
-        nextButton.gameObject.GetComponent<PassiveButton>().OnClick.AddListener((UnityAction)(() =>
+        nextButton.gameObject.GetComponent<CloseButtonConsoleBehaviour>().DestroyImmediate();
+        nextButton.gameObject.GetComponent<PassiveButton>().DestroyImmediate();
+
+        PassiveButton passiveButton = nextButton.gameObject.AddComponent<PassiveButton>();
+        passiveButton.HoverSound = __instance.BackButton.GetComponent<PassiveButton>().HoverSound;
+        passiveButton.ClickSound = __instance.BackButton.GetComponent<PassiveButton>().ClickSound;
+
+        passiveButton.activeSprites = nextButton.transform.FindChild("Active").gameObject;
+        passiveButton.inactiveSprites = nextButton.transform.FindChild("Inactive").gameObject;
+        passiveButton.OnClick.AddListener((UnityAction)(() =>
         {
             currentSelectedMod += 1;
         }));
 
         GameObject backButton = GameObject.Instantiate(nextButton, __instance.BackButton.transform.parent).gameObject;
-        nextButton.transform.localPosition = new Vector3(-4.4209f, -0.4037f, -25f);
-        nextButton.name = "BackButton";
-        nextButton.transform.FindChild("Active").gameObject.GetComponent<SpriteRenderer>().flipX = nextButton.transform.FindChild("Inactive").gameObject.GetComponent<SpriteRenderer>().flipX = true;
-        backButton.gameObject.GetComponent<PassiveButton>().OnClick.RemoveAllListeners();
+        backButton.transform.localPosition = new Vector3(-4.4209f, 1.5272f, -25f);
+        backButton.name = "LeftArrowButton";
+        backButton.gameObject.GetComponent<CloseButtonConsoleBehaviour>().Destroy();
+        backButton.transform.FindChild("Active").gameObject.GetComponent<SpriteRenderer>().flipX = backButton.transform.FindChild("Inactive").gameObject.GetComponent<SpriteRenderer>().flipX = true;
         backButton.gameObject.GetComponent<PassiveButton>().OnClick.AddListener((UnityAction)(() =>
         {
             currentSelectedMod -= 1;
         }));
-
-        __instance.GamePresetsButton.gameObject.SetActive(false);
     }
-
-    /*public static void CreateOptionsFor(GameSettingMenu __instance, ToggleOption togglePrefab, NumberOption numberPrefab, StringOption stringPrefab, Transform container,
-        List<IModdedOption> options)
-    {
-        foreach (IModdedOption option in options)
-        {
-            if (option.AdvancedRole is not null || option.OptionBehaviour)
-            {
-                continue;
-            }
-
-            OptionBehaviour newOption = option.CreateOption(togglePrefab, numberPrefab, stringPrefab, container);
-            __instance.AllItems = __instance.AllItems.AddItem(newOption.transform).ToArray();
-        }
-    }*/
 }
