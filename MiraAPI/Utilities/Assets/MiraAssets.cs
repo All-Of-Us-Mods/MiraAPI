bf5d9f4eefcd06265435ed405de534b2db96ec96
ang-xd <angxdxdd@gmail.com>
2024-06-20 14:56:31 -0400

    wip pagination

--- MiraAPI/Utilities/Assets/MiraAssets.cs
+++ MiraAPI/Utilities/Assets/MiraAssets.cs
@@ -3,5 +3,8 @@
     public static class MiraAssets
     {
         public static readonly LoadableResourceAsset Empty = new("", "");
+
+        public static readonly LoadableResourceAsset NextButton = new("NextButton.png", "MiraAPI.Resources.");
+        public static readonly LoadableResourceAsset NextButtonActive = new("NextButtonActive.png", "MiraAPI.Resources.");
     }
 }
