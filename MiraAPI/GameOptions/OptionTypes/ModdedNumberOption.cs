f9a587257bbc9069cdc74e5442a146391b3c8126
XtraCube <72575280+XtraCube@users.noreply.github.com>
2024-08-22 13:03:27 -0400

    fix format strings for options

--- MiraAPI/GameOptions/OptionTypes/ModdedNumberOption.cs
+++ MiraAPI/GameOptions/OptionTypes/ModdedNumberOption.cs
@@ -37,7 +37,7 @@ public class ModdedNumberOption : ModdedOption<float>
         data.Value = Value;
         data.Increment = Increment;
         data.ValidRange = new FloatRange(Min, Max);
-        data.FormatString = Increment % 1 == 0 ? "0" : "0.0";
+        data.FormatString = Increment % 1 == 0 && Value % 1 == 0  && Min % 1 == 0  && Max % 1 == 0 ? "0" : "0.0";
         data.ZeroIsInfinity = ZeroInfinity;
         data.SuffixType = SuffixType;
         data.OptionName = FloatOptionNames.Invalid;
@@ -54,7 +54,7 @@ public class ModdedNumberOption : ModdedOption<float>
         numberOption.Value = Value;
         numberOption.Increment = Increment;
         numberOption.ValidRange = new FloatRange(Min, Max);
-        numberOption.FormatString = "0";
+        numberOption.FormatString = ((FloatGameSetting)Data).FormatString;
         numberOption.ZeroIsInfinity = ZeroInfinity;
         numberOption.SuffixType = SuffixType;
         numberOption.floatOptionName = FloatOptionNames.Invalid;
