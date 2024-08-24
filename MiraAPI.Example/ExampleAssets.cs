7480217d6ca6c1574304809cbd1f661b12c956eb
XtraCube <72575280+XtraCube@users.noreply.github.com>
2024-08-24 11:30:14 -0400

    wrong file type in example assets oops

--- MiraAPI.Example/ExampleAssets.cs
+++ MiraAPI.Example/ExampleAssets.cs
@@ -5,5 +5,5 @@ namespace MiraAPI.Example;
 public static class ExampleAssets
 {
     public static LoadableResourceAsset ExampleButton { get; } = new("MiraAPI.Example.Resources.ExampleButton.png");
-    public static LoadableResourceAsset Banner { get; } = new("MiraAPI.Example.Resources.FortniteBanner.png");
+    public static LoadableResourceAsset Banner { get; } = new("MiraAPI.Example.Resources.FortniteBanner.jpeg");
 }
