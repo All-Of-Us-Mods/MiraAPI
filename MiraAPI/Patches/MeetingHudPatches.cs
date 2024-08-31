8fe58ca6cad72edddc2883a0244266cfd31169cf
XtraCube <72575280+XtraCube@users.noreply.github.com>
2024-08-31 15:12:39 -0400

    reset buttons on meeting start

--- MiraAPI/Patches/MeetingHudPatches.cs
+++ MiraAPI/Patches/MeetingHudPatches.cs
@@ -3,9 +3,28 @@ using MiraAPI.Hud;
 
 namespace MiraAPI.Patches;
 
+/// <summary>
+/// Harmony patches for the MeetingHud class.
+/// </summary>
 [HarmonyPatch(typeof(MeetingHud))]
 public static class MeetingHudPatches
 {
+    /// <summary>
+    /// Resets the cooldown and effect of all custom buttons when the meeting starts.
+    /// </summary>
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(MeetingHud.Start))]
+    public static void StartPostfix()
+    {
+        foreach (var customActionButton in CustomButtonManager.CustomButtons)
+        {
+            customActionButton.ResetCooldownAndOrEffect();
+        }
+    }
+
+    /// <summary>
+    /// Resets the cooldown and effect of all custom buttons when the voting is complete.
+    /// </summary>
     [HarmonyPostfix]
     [HarmonyPatch(nameof(MeetingHud.VotingComplete))]
     public static void VotingCompletePostfix()
