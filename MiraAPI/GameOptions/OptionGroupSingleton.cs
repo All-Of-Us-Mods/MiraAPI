0f26cadd9af89e7fe89932efbabe262f8618c7c6
XtraCube <72575280+XtraCube@users.noreply.github.com>
2024-09-01 16:07:36 -0400

    clean up OptionGroupSingleton.cs

--- MiraAPI/GameOptions/OptionGroupSingleton.cs
+++ MiraAPI/GameOptions/OptionGroupSingleton.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace MiraAPI.GameOptions;
 
@@ -7,13 +6,12 @@ namespace MiraAPI.GameOptions;
 /// Singleton for option groups.
 /// </summary>
 /// <typeparam name="T">The option group type.</typeparam>
-public class OptionGroupSingleton<T> where T : AbstractOptionGroup
+public static class OptionGroupSingleton<T> where T : AbstractOptionGroup
 {
     private static T? _instance;
 
     /// <summary>
     /// Gets the instance of the option group.
     /// </summary>
-    /// <exception cref="InvalidOperationException">Can't</exception>
     public static T Instance => _instance ??= ModdedOptionsManager.Groups.OfType<T>().Single();
 }
