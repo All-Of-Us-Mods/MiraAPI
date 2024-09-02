ae10917f2a45fd9abd4318739277b796e90f1090
XtraCube <72575280+XtraCube@users.noreply.github.com>
2024-09-02 15:22:56 -0400

    hidefromil2cpp

--- MiraAPI.Example/MiraDebugWindow.cs
+++ MiraAPI.Example/MiraDebugWindow.cs
@@ -1,6 +1,7 @@
-﻿using Reactor.Utilities.Attributes;
+﻿using System;
+using Il2CppInterop.Runtime.Attributes;
+using Reactor.Utilities.Attributes;
 using Reactor.Utilities.ImGui;
-using System;
 using UnityEngine;
 
 namespace MiraAPI.Example;
@@ -8,6 +9,7 @@ namespace MiraAPI.Example;
 [RegisterInIl2Cpp]
 public class MiraDebugWindow(IntPtr cppPtr) : MonoBehaviour(cppPtr)
 {
+    [HideFromIl2Cpp]
     public DragWindow DebuggingWindow { get; } = new(
         new Rect(10, 10, 0, 0),
         "MIRA API DEBUGGING",
