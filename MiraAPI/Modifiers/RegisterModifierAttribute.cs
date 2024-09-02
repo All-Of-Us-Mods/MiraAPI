3d9bbc0833f450aea8cdf58df327974ddd2eb80a
XtraCube <72575280+XtraCube@users.noreply.github.com>
2024-09-02 13:01:34 -0400

    comments

--- MiraAPI/Modifiers/RegisterModifierAttribute.cs
+++ MiraAPI/Modifiers/RegisterModifierAttribute.cs
@@ -2,5 +2,8 @@
 
 namespace MiraAPI.Modifiers;
 
+/// <summary>
+/// Marks a class as a modifier that can be registered with the <see cref="ModifierManager"/>.
+/// </summary>
 [AttributeUsage(AttributeTargets.Class)]
 public class RegisterModifierAttribute : Attribute;
--- MiraAPI/Networking/MiraRpc.cs
+++ MiraAPI/Networking/MiraRpc.cs
@@ -1,11 +1,37 @@
 ﻿namespace MiraAPI.Networking;
 
+/// <summary>
+/// Mira RPCs.
+/// </summary>
 public enum MiraRpc : uint
 {
+    /// <summary>
+    /// Syncs the game options.
+    /// </summary>
     SyncGameOptions,
+
+    /// <summary>
+    /// Syncs the role options.
+    /// </summary>
     SyncRoleOptions,
+
+    /// <summary>
+    /// Adds a modifier to a player.
+    /// </summary>
     AddModifier,
+
+    /// <summary>
+    /// Removes a modifier from a player.
+    /// </summary>
     RemoveModifier,
+
+    /// <summary>
+    /// Syncs all modifiers at once.
+    /// </summary>
     SyncModifiers,
+
+    /// <summary>
+    /// Custom Murder RPC.
+    /// </summary>
     CustomMurder,
 }
