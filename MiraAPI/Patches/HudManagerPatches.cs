7471a43d396f8e7d8db8107666c88320ff961490
XtraCube <72575280+XtraCube@users.noreply.github.com>
2024-09-14 19:29:14 -0400

    fix white button flashing issue

--- MiraAPI/Patches/HudManagerPatches.cs
+++ MiraAPI/Patches/HudManagerPatches.cs
@@ -89,6 +89,7 @@ public static class HudManagerPatches
     [HarmonyPatch(nameof(HudManager.SetHudActive), typeof(PlayerControl), typeof(RoleBehaviour), typeof(bool))]
     public static void SetHudActivePostfix(HudManager __instance, PlayerControl localPlayer, RoleBehaviour role, bool isActive)
     {
+        __instance.AdminButton.ToggleVisible(isActive && role.IsImpostor && GameOptionsManager.Instance.CurrentGameOptions.GameMode == AmongUs.GameOptions.GameModes.HideNSeek);
         if (!localPlayer.Data)
         {
             return;
