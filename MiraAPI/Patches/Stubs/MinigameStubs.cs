68afe58678d1cffe40c2ed61a8ab1241960db846
XtraCube <72575280+XtraCube@users.noreply.github.com>
2024-09-22 10:52:48 -0400

    custom player menu in mira

---
+++ MiraAPI/Patches/Stubs/MinigameStubs.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using HarmonyLib;
+
+namespace MiraAPI.Patches.Stubs;
+
+/// <summary>
+/// Reverse patches for Minigames.
+/// </summary>
+[HarmonyPatch]
+public static class MinigameStubs
+{
+    /// <summary>
+    /// Reverse patch for Minigame.Begin.
+    /// </summary>
+    /// <param name="instance">The Minigame instance.</param>
+    /// <param name="task">Associated PlayerTask.</param>
+    [HarmonyReversePatch]
+    [HarmonyPatch(typeof(Minigame), nameof(Minigame.Begin))]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void Begin(Minigame instance, PlayerTask? task)
+    {
+        // nothing needed
+    }
+}
