f10431f5b152988be045b0b2c3bfb696cdc94f09
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-09-20 09:49:51 -0500

    Add Arabic, Greek, Hebrew, and Lit-Chinese

--- MiraAPI/Translation/MiraLocaleManager.cs
+++ MiraAPI/Translation/MiraLocaleManager.cs
@@ -45,6 +45,10 @@ public static class MiraLocaleManager
         { MiraLanguage.Swedish, "sv_SE" }, // Custom
         { MiraLanguage.Lithuanian, "lt_LT" }, // Custom
         { MiraLanguage.Czech, "cs_CZ" }, // Custom
+        { MiraLanguage.LiteraryChinese, "lzh_CN" }, // Custom
+        { MiraLanguage.Greek, "el_GR" }, // Custom
+        { MiraLanguage.Arabic, "arb" }, // Custom
+        { MiraLanguage.Hebrew, "heb" }, // Custom
     };
     public static Dictionary<MiraLanguage, string> LangCultureList { get; } = new()
     {
@@ -69,6 +73,10 @@ public static class MiraLocaleManager
         { MiraLanguage.Swedish, "sv-SE" }, // Custom
         { MiraLanguage.Lithuanian, "lt-LT" }, // Custom
         { MiraLanguage.Czech, "cs-CZ" }, // Custom
+        { MiraLanguage.LiteraryChinese, "lzh-CN" }, // Custom
+        { MiraLanguage.Greek, "el-GR" }, // Custom
+        { MiraLanguage.Arabic, "arb" }, // Custom
+        { MiraLanguage.Hebrew, "heb" }, // Custom
     };
 
     /// <summary>
